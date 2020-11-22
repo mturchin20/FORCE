cb46d56bcf1788a630dda5fe3cfaccc78d842205
Michael Turchin <mturchin20@gmail.com>
2020-11-21 22:57:48 -0500

    Updating InterPath.Workbook.vs1.sh -- Revisions Rnd1 (Simulation code setup)

---
+++ SimulationMain.Revisions.vs1.R
@@ -0,0 +1,158 @@
+library(MAPITR)
+
+set.seed(11151990)
+
+args <- commandArgs()
+print(args)
+rho <- args[1]
+pc.var = 0.10 
+#Variance Explained by PCs (Population Structure)
+
+#X <- read.table("/users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2AdditiveMdls/Simulations/Data/ukb_chrAll_v3.African.QCed.reqDrop.QCed.dropRltvs.PCAdrop.sort.ImptHRC.dose.100geno.raw.edit.Chr16.txt.gz", header=T);
+X <- read.table("/users/mturchin/data/ukbiobank_jun17/subsets/African/African/Imputation/mturchin20/ukb_chrAll_v3.African}QCed.reqDrop.QCed.dropRltvs.PCAdrop.sort.ImptHRC.dose.100geno.ForSimulations.chr16.raw.edit.Rheaders.gz", header=T);
+##X.Headers <- read.table("/users/mturchin/data/ukbiobank_jun17/subsets/African/African/Imputation/mturchin20/ukb_chrAll_v3.African}QCed.reqDrop.QCed.dropRltvs.PCAdrop.sort.ImptHRC.dose.100geno.ForSimulations.chr16.raw.edit.SNPIDs.Rformat.txt", header=F);
+Genes <- read.table("/users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2AdditiveMdls/Simulations/Data/ukb_chrAll_v3.African.QCed.reqDrop.QCed.dropRltvs.PCAdrop.sort.ImptHRC.dose.100geno.bim.AnnovarFormat.TableAnnovar.AAFix.hg19_multianno.GeneSNPs.SemiColonSplit.wRowPos.Regions.ExonicPlus20kb.SimFormat.Chr16.Rformat.txt", header=F);
+#PCs <- read.table("", header=F);
+
+#maf=apply(X, 2, mean)/2
+#X = X[,maf>0.01]; dim(X)
+Xmean=apply(X, 2, mean); Xsd=apply(X, 2, sd); X=t((t(X)-Xmean)/Xsd)
+
+ind = nrow(X); nsnp = ncol(X)
+
+### Define the Simulation Parameters ###
+n.datasets = 100 #Total Number of Simulations 
+pve = 0.6; #Heritability of the trait
+rho = 0.8; #Proportion of the heritability caused by additive effects {0.8, 0.5}
+
+### Set Up Causal Pathways in Three Groups
+ngenes = 20; #number of genes needed to get around 1000 SNPs pulled for G1, G2, and G3
+ncausal1 = .05; ncausal2 = .2 #Percent of SNPs needed for G1 and G2
+ncausal3 = 1-(ncausal1+ncausal2) #Remaining SNPs to be allocated to G3
+
+### Create a list to save the final Results ###
+pval_mat = matrix(nrow = npthwy,ncol = n.datasets); rownames(pval_mat) = names(gene_list)
+G1_snps = matrix(nrow = ncausal1,ncol = n.datasets)
+G2_snps = matrix(nrow = ncausal2,ncol = n.datasets)
+
+### Run the Analysis ###
+for(j in 1:n.datasets){
+  
+  #Select Causal Pathways
+  gene.ids = 1:nrow(Genes)
+  genes.pulled.ids <- sample(gene.ids, 1:nrow(Genes), replace=F);
+  genes.pulled <- Genes[genes.pulled.ids,];
+  
+
+  genes.pulled.SNPs <-
+  genes.pulled.SNPs.ids <- 1:length(genes.pulled.SNPs)
+  s1=sample(genes.pulled.SNPs.ids, ncausal1, replace=F)
+  s2=sample(genes.pulled.SNPs.ids[-s1], ncausal2, replace=F)
+  s3=sample(genes.pulled.SNPs.ids[-c(s1,s2)], ncausal3, replace=F)
+
+
+
+  ### Simulate the Additive Effects ###
+  SNPs.additive = c(s1,s2,s3);
+  Xmarginal = X[,SNPs.additive]
+  beta=rnorm(dim(Xmarginal)[2])
+  y_marginal=c(Xmarginal%*%beta)
+  beta=beta*sqrt(pve*rho/var(y_marginal))
+  y_marginal=Xmarginal%*%beta
+  
+  ### Simulate Pairwise Interaction matrix ###
+  Xepi = c(); b = c()
+  for(i in 1:ncausal1){
+      Xepi = cbind(Xepi,X[,s1[i]]*X[,s2]) 
+    }
+  }
+  
+  ### Simulate the Pairwise Effects ###
+  beta=rnorm(dim(Xepi)[2])
+  y_epi=c(Xepi%*%beta)
+  beta=beta*sqrt(pve*(1-rho)/var(y_epi))
+  y_epi=Xepi%*%beta
+  
+  ### Define the effects of the PCs ###
+  beta=rnorm(dim(PCs)[2])
+  y_pcs=c(PCs%*%beta)
+  beta=beta*sqrt(pc.var/var(z_pcs))
+  y_pcs=PCs%*%beta
+  
+  ### Simulate the (Environmental) Error/Noise ###
+  y_err=rnorm(ind)
+  y_err=y_err*sqrt((1-pve-pc.var)/var(y_err))
+  
+  ### Simulate the Total Phenotypes ###
+  y=y_marginal+y_epi+y_pcs+y_err
+  y=(y-mean(y))/sd(y)
+  
+  ### Check dimensions ###
+  dim(X); length(y)
+  
+  ######################################################################################
+  ######################################################################################
+  ######################################################################################
+  
+  ### Rewrite the Pathway List to Give Column IDs Instead of SNP Names ###
+  regions = matrix(nrow = nsnp, ncol = length(gene_list))
+  rownames(regions) = colnames(X)
+  colnames(regions) = names(gene_list)
+  for(i in 1:length(gene_list)){
+      regions[which(colnames(X)%in%gene_list[[i]]),i] = 1
+  }
+  
+  ######################################################################################
+  ######################################################################################
+  ######################################################################################
+  
+  ### Set the number of cores ###
+  cores = detectCores()
+  
+  ### Run InterPath ###
+  ptm <- proc.time() #Start clock
+  vc.mod = InterPath(t(X),y,regions,cores = cores)
+  proc.time() - ptm #Stop clock
+  
+  ### Apply Davies Exact Method ###
+  vc.ts = vc.mod$Est
+  names(vc.ts) = colnames(regions)
+  
+  pvals = c()
+  for(i in 1:length(vc.ts)){
+      lambda = sort(vc.mod$Eigenvalues[,i],decreasing = T)
+      Davies_Method = davies(vc.mod$Est[i], lambda = lambda, acc=1e-8)
+      pvals[i] = 2*min(1-Davies_Method$Qq,Davies_Method$Qq)
+      names(pvals)[i] = names(vc.ts[i])
+  }
+  
+  ######################################################################################
+  ######################################################################################
+  ######################################################################################
+  
+  ### Find power for the first group of SNPs ###
+  Pthwys_1 = colnames(regions)[s1]
+  
+  ### Find power for the second group of SNPs ###
+  Pthwys_2 = colnames(regions)[s2]
+  
+  ######################################################################################
+  ######################################################################################
+  ######################################################################################
+  
+  ### Save Results ###
+  pval_mat[,j] = pvals
+  G1_snps[,j] = Pthwys_1
+  G2_snps[,j] = Pthwys_2
+  
+  ### Report Status ###
+  cat("Completed Dataset", j, "\n", sep = " ")
+}
+
+#Save final Results
+Final = list(pval_mat,G1_snps,G2_snps)
+
+### Save the Results ###
+file = "/home/lcrawfo1/Results/InterPath/PopStruct_Sim_rho8_S1.RData"
+save(Final, file = file)
+
