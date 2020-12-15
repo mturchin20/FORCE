96795468c747d5a0c5cd55c95f002d54e3310c05
Michael Turchin <mturchin20@gmail.com>
2020-12-15 01:46:15 -0500

    Updating InterPath.Workbook.vs1.sh -- Revisions Rnd1 (Simulation code setup [starting runs of 3rd round])

--- InterPath.Workbook.vs1.sh
+++ InterPath.Workbook.vs1.sh
@@ -18971,28 +18971,65 @@ mkdir /users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2Additiv
 `cat <(echo "5 10 15 25" | perl -lane 'print join("\n", @F);') |
 `cat <(echo "2 5 7 12" | perl -lane 'print join("\n", @F);') |
 
-head -n 1 and tail -n 1 (head -n 1 only for the last run)
-(have runs going for the 5 10 etc group of .6, .8 pves, .25 and .5 rhos; only Brit; for 2 5 7, have .6, .8, .25, .5, only Brit; and for Afr have .6, .8, .25, .5 too
+#head -n 1 and tail -n 1 (head -n 1 only for the last run)
+#(have runs going for the 5 10 etc group of .6, .8 pves, .25 and .5 rhos; only Brit; for 2 5 7, have .6, .8, .25, .5, only Brit; and for Afr have .6, .8, .25, .5 too
 
-module load R/3.4.3_mkl gcc mpi/openmpi_4.0.5_icc; for j in `cat <(echo $UKBioBankPopsRnd2 | perl -lane 'print join("\n", @F);') | head -n 8 | head -n 8 | tail -n 8 | head -n 2 | head -n 1`; do
+compare PVE .6/.8, rho .5/.8, with either '20 40 50 80' and causal% .1/.25 (20 in ncausal1, 200 in total), or 2 5 7 12 (w/ 2 or 5, 2 in ncausal1, 10 in total) with .75/1 
+
+want like <100 SNPs probably in Epi
+
+British British.Ran4000 138503
+1 2 0 0 49 53 123 102 225
+2 2 0 0 43 56 117 99 216
+3 2 0 0 53 37 113 90 203
+4 2 3 0 9 66 123 75 198
+5 2 0 0 35 75 116 110 226
+1 1 0 0 49 90 84 139 223
+2 1 0 0 43 66 79 109 188
+3 1 0 0 53 94 27 147 174
+4 2 4 0 9 132 88 141 229
+5 1 0 0 35 132 79 167 246
+1 4 1 0 49 53 123 102 225
+2 2 0 0 43 56 117 99 216
+3 2 0 0 53 37 113 90 203
+4 4 10 0 9 66 123 75 198
+5 3 0 0 35 75 116 110 226
+1 1 1 0 49 90 84 139 223
+2 3 1 0 43 66 79 109 188
+3 1 0 0 53 94 27 147 174
+4 2 6 0 9 132 88 141 229
+5 1 0 0 35 132 79 167 246
+1 2 0 0 66 70 163 136 299
+2 3 0 0 57 76 157 133 290
+3 3 0 0 69 49 147 118 265
+4 5 6 0 12 89 166 101 267
+5 1 0 0 46 102 156 148 304
+1 3 0 0 66 119 112 185 297
+2 1 1 0 57 88 107 145 252
+3 3 0 0 69 125 33 194 227
+4 2 3 0 12 177 116 189 305
+5 4 0 0 46 178 107 224 331
+
+
+module load R/3.4.3_mkl gcc mpi/openmpi_4.0.5_icc; for j in `cat <(echo $UKBioBankPopsRnd2 | perl -lane 'print join("\n", @F);') | head -n 8 | head -n 8 | tail -n 8 | head -n 2 | tail -n 1`; do
 	ancestry1=`echo $j | perl -ane 'my @vals1 = split(/;/, $F[0]); print $vals1[0];'`; ancestry2=`echo $j | perl -ane 'my @vals1 = split(/;/, $F[0]); print $vals1[1];'`; AncSeed1=`echo $j | perl -ane 'my @vals1 = split(/;/, $F[0]); print $vals1[3];'`
 	echo $ancestry1 $ancestry2 $AncSeed1
 
-	for pve2 in `cat <(echo ".6 .8" | perl -lane 'print join("\n", @F);')`; do for rho2 in `cat <(echo ".25 .5 .8" | perl -lane 'print join("\n", @F);') | head -n 2`; do for pcvar2 in `cat <(echo "0 .1" | perl -lane 'print join("\n", @F);') | head -n 1`; do for ncaustot2 in `cat <(echo ".1 .25 .5 .75 1" | perl -lane 'print join("\n", @F);') | tail -n 2`; do for ncaus2a2 in `cat <(echo "2 5 7 12" | perl -lane 'print join("\n", @F);') | tail -n 2`; do
+	for pve2 in `cat <(echo ".6 .8" | perl -lane 'print join("\n", @F);')`; do for rho2 in `cat <(echo ".5 .8" | perl -lane 'print join("\n", @F);') | head -n 2`; do for pcvar2 in `cat <(echo "0 .1" | perl -lane 'print join("\n", @F);') | head -n 1`; do for ncaustot2 in `cat <(echo ".1 .25 .5 .75 1" | perl -lane 'print join("\n", @F);') | tail -n 2`; do for ncaus2a2 in `cat <(echo "2 5 7 12" | perl -lane 'print join("\n", @F);') | head -n 2`; do
 		for o in {1..5}; do
 			X_File1="/users/mturchin/data/ukbiobank_jun17/subsets/$ancestry1/$ancestry2/Imputation/mturchin20/ukb_chrAll_v3.${ancestry2}.QCed.reqDrop.QCed.dropRltvs.PCAdrop.sort.ImptHRC.dose.100geno.ForSimulations.chr16.raw.edit.Rheaders.gz";
 			Genes_File1="/users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2AdditiveMdls/Simulations/Data/ukb_chrAll_v3.${ancestry2}.QCed.reqDrop.QCed.dropRltvs.PCAdrop.sort.ImptHRC.dose.100geno.bim.TableAnnovar.AAFix.hg19_multianno.GeneSNPs.wRowPos.Regions.ExonicPlus20kb.SimFormat.Chr16.perSNPs.noGeneDups.GenesFormat.Rformat.txt"
 			Covars_File1="/users/mturchin/data/ukbiobank_jun17/subsets/$ancestry1/$ancestry2/mturchin20/ukb_chrAll_v3.${ancestry2}.QCed.pruned.QCed.dropRltvs.noX.PCAdrop.flashpca.pcs.wFullCovars.wAC.txt"
 			Genes_File2="/users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2AdditiveMdls/Simulations/Data/ukb_chrAll_v3.${ancestry2}.QCed.reqDrop.QCed.dropRltvs.PCAdrop.sort.ImptHRC.dose.100geno.bim.TableAnnovar.AAFix.hg19_multianno.GeneSNPs.wRowPos.Regions.ExonicPlus20kb.SimFormat.Chr16.perSNPs.noGeneDups.GenesFormat.Rformat.MAPITRformat.txt"
-			Seed1=`echo "$AncSeed1 + ($o * 2)" | bc -l`
 			Datasets1=1
 			PVE1=$pve2
 			Rho1=$rho2
 			PCs_var1=$pcvar2
 			ncausaltotal1=$ncaustot2
-			nCausal1a=3
+			nCausal1a=2
 			nCausal2a=$ncaus2a2
 			nCausal3a=`echo "25 - $nCausal1a - $nCausal2a" | bc -l`
+			Seed1=`echo "$AncSeed1 + ($o * 2) + ($pve2 * 10) + ($rho2 * 10) + ($pcvar2 * 10) + ($ncaustot2 * 10) + ($ncaus2a2 * 10) + ($nCausal1a * 10)" | bc -l`
 			Output1_Path="/users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2AdditiveMdls/Simulations/20201109Lorin/Null/Results/$ancestry2"
 			Output1_File1="${Output1_Path}/ukb_chrAll_v3.${ancestry2}.QCed.reqDrop.QCed.dropRltvs.PCAdrop.sort.ImptHRC.dose.100geno.ForSimulations.chr16.Results._${PVE1}_${Rho1}_${PCs_var1}_${ncausaltotal1}_${nCausal1a}_${nCausal2a}_${nCausal3a}.Run${o}"; Output1_Slurm1="${Output1_Path}/slurm/ukb_chrAll_v3.${ancestry2}.QCed.reqDrop.QCed.dropRltvs.PCAdrop.sort.ImptHRC.dose.100geno.ForSimulations.chr16.Results._${PVE1}_${Rho1}_${PCs_var1}_${ncausaltotal1}_${nCausal1a}_${nCausal2a}_${nCausal3a}.Run${o}"
 	
@@ -19019,32 +19056,37 @@ done
 #additive (null) analyses: .6, 1 (for additive only), PCs_var 0/.1
 #phantom epi (null+masking) analyses: .6, 1 (for additive only), PCs_var 0/.1 + masking -- only do for African, and only do for scenarios with a lot of epistasis already
 
-for j in `cat <(echo $UKBioBankPopsRnd2 | perl -lane 'print join("\n", @F);') | head -n 8 | head -n 8 | tail -n 8 | head -n 2 | tail -n 1`; do
+for j in `cat <(echo $UKBioBankPopsRnd2 | perl -lane 'print join("\n", @F);') | head -n 8 | head -n 8 | tail -n 8 | head -n 2`; do
 	ancestry1=`echo $j | perl -ane 'my @vals1 = split(/;/, $F[0]); print $vals1[0];'`; ancestry2=`echo $j | perl -ane 'my @vals1 = split(/;/, $F[0]); print $vals1[1];'`; AncSeed1=`echo $j | perl -ane 'my @vals1 = split(/;/, $F[0]); print $vals1[3];'`
 	echo $ancestry1 $ancestry2 $AncSeed1
 
-	for pve2 in `cat <(echo ".6" | perl -lane 'print join("\n", @F);')`; do for rho2 in `cat <(echo ".5 .8" | perl -lane 'print join("\n", @F);') | head -n 1`; do for pcvar2 in `cat <(echo "0 .1" | perl -lane 'print join("\n", @F);') | head -n 1`; do for ncaustot2 in `cat <(echo ".1 .25 .5 .75 1" | perl -lane 'print join("\n", @F);') | tail -n 2`; do for ncaus2a2 in `cat <(echo "5 10 15 20" | perl -lane 'print join("\n", @F);') | tail -n 2 | head -n 1`; do
+	for pve2 in `cat <(echo ".6 .8" | perl -lane 'print join("\n", @F);')`; do for rho2 in `cat <(echo ".5 .8" | perl -lane 'print join("\n", @F);') | head -n 1`; do for pcvar2 in `cat <(echo "0 .1" | perl -lane 'print join("\n", @F);') | head -n 1`; do for ncaustot2 in `cat <(echo ".1 .25 .5 .75 1" | perl -lane 'print join("\n", @F);') | tail -n 2`; do for ncaus2a2 in `cat <(echo "2 5 7 12" | perl -lane 'print join("\n", @F);') | tail -n 2`; do
 		for o in {1..5}; do
 			PVE1=$pve2
 			Rho1=$rho2
 			PCs_var1=$pcvar2
 			ncausaltotal1=$ncaustot2
-			nCausal1a=5
+			nCausal1a=3
 			nCausal2a=$ncaus2a2
-			nCausal3a=`echo "50 - $nCausal1a - $nCausal2a" | bc -l`
+			nCausal3a=`echo "25 - $nCausal1a - $nCausal2a" | bc -l`
 			Output1_Path="/users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2AdditiveMdls/Simulations/20201109Lorin/Null/Results/$ancestry2"
 			Output1_File1="${Output1_Path}/ukb_chrAll_v3.${ancestry2}.QCed.reqDrop.QCed.dropRltvs.PCAdrop.sort.ImptHRC.dose.100geno.ForSimulations.chr16.Results._${PVE1}_${Rho1}_${PCs_var1}_${ncausaltotal1}_${nCausal1a}_${nCausal2a}_${nCausal3a}.Run${o}"; Output1_Slurm1="${Output1_Path}/slurm/ukb_chrAll_v3.${ancestry2}.QCed.reqDrop.QCed.dropRltvs.PCAdrop.sort.ImptHRC.dose.100geno.ForSimulations.chr16.Results._${PVE1}_${Rho1}_${PCs_var1}_${ncausaltotal1}_${nCausal1a}_${nCausal2a}_${nCausal3a}.Run${o}"
 			GenesPulled1="${Output1_File1}.Results.nGenes.txt"
 			Results1="${Output1_File1}.Results.Output.txt"
-	
+			Stats1="${Output1_File1}.Results.statistics.txt"
+
 			NumGenes=`cat $Results1 | wc | awk '{ print $1 }'`
 			pValBonf=`echo ".05 / $NumGenes" | bc -l`;        
 	
 			TruePos=`join <(cat $Results1 | sort -k 1,1) <(cat $GenesPulled1 | grep Epi | awk '{ print $1 }' | sort ) | awk -v pValBonf=$pValBonf '{ if ($2 < pValBonf) { print $0 } } ' | wc | awk '{ print $1 }'` 
 			FalsePos=`join -v 1 <(cat $Results1 | sort -k 1,1) <(cat $GenesPulled1 | grep Epi | awk '{ print $1 }' | sort ) | awk -v pValBonf=$pValBonf '{ if ($2 < pValBonf) { print $0 } } ' | wc | awk '{ print $1 }'`
 			FalsePosAdd=`join <(cat $Results1 | sort -k 1,1) <(cat $GenesPulled1 | grep Add | awk '{ print $1 }' | sort ) | awk -v pValBonf=$pValBonf '{ if ($2 < pValBonf) { print $0 } } ' | wc | awk '{ print $1 }'`
-	
-			echo $o $TruePos $FalsePos $FalsePosAdd
+			numEpi1SNPs=`cat $Stats1 | head -n 3 | tail -n 1 | awk '{ print $1 }'`; numEpi2SNPs=`cat $Stats1 | head -n 4 | tail -n 1 | awk '{ print $1 }'`; numAdd1SNPs=`cat $Stats1 | head -n 5| tail -n 1| awk '{ print $1 }'` 
+			numEpiTotal=`echo "$numEpi1SNPs + $numEpi2SNPs" | bc -l`; numSNPsTotal=`echo "$numEpiTotal + $numAdd1SNPs" | bc -l`;
+
+			echo $o $TruePos $FalsePos $FalsePosAdd $numEpi1SNPs $numEpi2SNPs $numAdd1SNPs $numEpiTotal $numSNPsTotal
+			
+			R -q -e "Data1 <- read.table(\"$Results1\", header=T); Data2 <- read.table(\"$GenesPulled1\", header=F); TrueGenes <- Data2[grep(\"Epi\", Data2[,2]),]; 
 
 		done;	
 	done; done; done; done; done;
