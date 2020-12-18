f8e746741a456b34bbd344d28637bbc7af83f251
Michael Turchin <mturchin20@gmail.com>
2020-12-17 23:27:09 -0500

    Updating InterPath.Workbook.vs1.sh -- Revisions Rnd1 (Simulation code setup [switching to vs4.dev, where I'm going to try and match Greg's setup with each pathway/gene interacting with a different background set of genes])

--- InterPath.Workbook.vs1.sh
+++ InterPath.Workbook.vs1.sh
@@ -18780,12 +18780,14 @@ cp -p /users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2Additiv
 cp -p /users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2AdditiveMdls/Simulations/20201109Lorin/SimulationMain.Revisions.wPCs.vs1.dev.R /users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2AdditiveMdls/Simulations/20201109Lorin/SimulationMain.Revisions.vs2.R 
 cp -p /users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2AdditiveMdls/Simulations/20201109Lorin/SimulationMain.Revisions.vs2.R /users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2AdditiveMdls/Simulations/20201109Lorin/SimulationMain.Revisions.vs3.R
 cp -p /users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2AdditiveMdls/Simulations/20201109Lorin/SimulationMain.Revisions.vs3.R /users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2AdditiveMdls/Simulations/20201109Lorin/SimulationMain.Revisions.vs3.dev.R
+cp -p /users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2AdditiveMdls/Simulations/20201109Lorin/SimulationMain.Revisions.vs3.dev.R /users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2AdditiveMdls/Simulations/20201109Lorin/SimulationMain.Revisions.vs4.dev.R
 ln /users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2AdditiveMdls/Simulations/20201109Lorin/SimulationMain.Revisions.vs1.R /users/mturchin/LabMisc/RamachandranLab/InterPath/SimulationMain.Revisions.vs1.R
 ln /users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2AdditiveMdls/Simulations/20201109Lorin/SimulationMain.Revisions.wPCs.vs1.R /users/mturchin/LabMisc/RamachandranLab/InterPath/SimulationMain.Revisions.wPCs.vs1.R
 ln /users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2AdditiveMdls/Simulations/20201109Lorin/SimulationMain.Revisions.wPCs.vs1.dev.R /users/mturchin/LabMisc/RamachandranLab/InterPath/SimulationMain.Revisions.wPCs.vs1.dev.R
 ln /users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2AdditiveMdls/Simulations/20201109Lorin/SimulationMain.Revisions.vs2.R /users/mturchin/LabMisc/RamachandranLab/InterPath/SimulationMain.Revisions.vs2.R
 ln /users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2AdditiveMdls/Simulations/20201109Lorin/SimulationMain.Revisions.vs3.R /users/mturchin/LabMisc/RamachandranLab/InterPath/SimulationMain.Revisions.vs3.R
 ln /users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2AdditiveMdls/Simulations/20201109Lorin/SimulationMain.Revisions.vs3.dev.R /users/mturchin/LabMisc/RamachandranLab/InterPath/SimulationMain.Revisions.vs3.dev.R
+ln /users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2AdditiveMdls/Simulations/20201109Lorin/SimulationMain.Revisions.vs4.dev.R /users/mturchin/LabMisc/RamachandranLab/InterPath/SimulationMain.Revisions.vs4.dev.R
 
 #From: https://stackoverflow.com/questions/56735768/how-can-i-pass-arguments-to-an-rscript-i-have-in-my-desktop, https://stackoverflow.com/questions/22906804/matrix-expression-causes-error-requires-numeric-complex-matrix-vector-arguments, https://swcarpentry.github.io/r-novice-inflammation/05-cmdline/, https://stackoverflow.com/questions/21969145/why-or-when-is-rscript-or-littler-better-than-r-cmd-batch, https://stackoverflow.com/questions/5234117/how-to-drop-columns-by-name-in-a-data-frame
 #Genes = Pathways, X SNPs across L genes, X SNPs across groups G1, G2, and G3, all three have additive effects, G1 and G2 have interaction effects
@@ -19016,12 +19018,13 @@ British British.Ran4000 138503
 5th cont'd: switched to the 1:20 genes analyzed setup now. First run is with the: '.05/.1/.25 % causal, 10 and 20 w/ 100 max now' setup
 30 & 40 w/ 100 max; 60 & 80 w/ 200 max
 100 & 140 w/ 300 max; 40, 100 & 140, 350 max
+2, 5 * 10 w/ 30 max; 20, 300 & 400, 600 max
 
 module load R/3.4.3_mkl gcc mpi/openmpi_4.0.5_icc; for j in `cat <(echo $UKBioBankPopsRnd2 | perl -lane 'print join("\n", @F);') | head -n 8 | head -n 8 | tail -n 8 | head -n 2`; do
 	ancestry1=`echo $j | perl -ane 'my @vals1 = split(/;/, $F[0]); print $vals1[0];'`; ancestry2=`echo $j | perl -ane 'my @vals1 = split(/;/, $F[0]); print $vals1[1];'`; AncSeed1=`echo $j | perl -ane 'my @vals1 = split(/;/, $F[0]); print $vals1[3];'`
 	echo $ancestry1 $ancestry2 $AncSeed1
 
-	for pve2 in `cat <(echo ".6 .8" | perl -lane 'print join("\n", @F);') | tail -n 1`; do for rho2 in `cat <(echo ".5 .8" | perl -lane 'print join("\n", @F);') | head -n 1`; do for pcvar2 in `cat <(echo "0 .1" | perl -lane 'print join("\n", @F);') | head -n 1`; do for ncaustot2 in `cat <(echo ".05 .1 .25 .5 .75 1" | perl -lane 'print join("\n", @F);') | head -n 3`; do for ncaus2a2 in `cat <(echo "2 5 7 100 140" | perl -lane 'print join("\n", @F);') | tail -n 2`; do
+	for pve2 in `cat <(echo ".6 .8" | perl -lane 'print join("\n", @F);') | tail -n 1`; do for rho2 in `cat <(echo ".5 .8" | perl -lane 'print join("\n", @F);') | head -n 1`; do for pcvar2 in `cat <(echo "0 .1" | perl -lane 'print join("\n", @F);') | head -n 1`; do for ncaustot2 in `cat <(echo ".05 .1 .25 .5 .75 1" | perl -lane 'print join("\n", @F);') | head -n 3`; do for ncaus2a2 in `cat <(echo "2 5 7 300 400" | perl -lane 'print join("\n", @F);') | tail -n 2`; do
 		for o in {1..5}; do
 			X_File1="/users/mturchin/data/ukbiobank_jun17/subsets/$ancestry1/$ancestry2/Imputation/mturchin20/ukb_chrAll_v3.${ancestry2}.QCed.reqDrop.QCed.dropRltvs.PCAdrop.sort.ImptHRC.dose.100geno.ForSimulations.chr16.raw.edit.Rheaders.gz";
 			Genes_File1="/users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2AdditiveMdls/Simulations/Data/ukb_chrAll_v3.${ancestry2}.QCed.reqDrop.QCed.dropRltvs.PCAdrop.sort.ImptHRC.dose.100geno.bim.TableAnnovar.AAFix.hg19_multianno.GeneSNPs.wRowPos.Regions.ExonicPlus20kb.SimFormat.Chr16.perSNPs.noGeneDups.GenesFormat.Rformat.txt"
@@ -19032,21 +19035,18 @@ module load R/3.4.3_mkl gcc mpi/openmpi_4.0.5_icc; for j in `cat <(echo $UKBioBa
 			Rho1=$rho2
 			PCs_var1=$pcvar2
 			ncausaltotal1=$ncaustot2
-			nCausal1a=40
+			nCausal1a=20
 			nCausal2a=$ncaus2a2
-			nCausal3a=`echo "350 - $nCausal1a - $nCausal2a" | bc -l`
+			nCausal3a=`echo "600 - $nCausal1a - $nCausal2a" | bc -l`
 			Seed1=`echo "$AncSeed1 + ($o * 2) + ($pve2 * 10) + ($rho2 * 10) + ($pcvar2 * 10) + ($ncaustot2 * 10) + ($ncaus2a2 * 10) + ($nCausal1a * 10)" | bc -l`
 			Output1_Path="/users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2AdditiveMdls/Simulations/20201109Lorin/Null/Results/$ancestry2"
 			Output1_File1="${Output1_Path}/ukb_chrAll_v3.${ancestry2}.QCed.reqDrop.QCed.dropRltvs.PCAdrop.sort.ImptHRC.dose.100geno.ForSimulations.chr16.Results._${PVE1}_${Rho1}_${PCs_var1}_${ncausaltotal1}_${nCausal1a}_${nCausal2a}_${nCausal3a}.Run${o}"; Output1_Slurm1="${Output1_Path}/slurm/ukb_chrAll_v3.${ancestry2}.QCed.reqDrop.QCed.dropRltvs.PCAdrop.sort.ImptHRC.dose.100geno.ForSimulations.chr16.Results._${PVE1}_${Rho1}_${PCs_var1}_${ncausaltotal1}_${nCausal1a}_${nCausal2a}_${nCausal3a}.Run${o}"
 	
-			sbatch -t 72:00:00 -n 2 -N 1-1 --mem 30g --account=ccmb-condo -o ${Output1_Slurm1}.slurm.output -e ${Output1_Slurm1}.slurm.error --comment "Sims $ancestry2 $o $PVE1 $Rho1 $PCs_var1 $ncausaltotal1 $nCausal1a $nCausal2a $nCausal3a" <(echo -e '#!/bin/sh'; echo -e "\nRscript /users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2AdditiveMdls/Simulations/20201109Lorin/SimulationMain.Revisions.vs3.dev.R $X_File1 $Genes_File1 $Covars_File1 $Genes_File2 $Output1_File1 $Seed1 $Datasets1 $PVE1 $Rho1 $PCs_var1 $ncausaltotal1 $nCausal1a $nCausal2a $nCausal3a")
-#			echo Rscript /users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2AdditiveMdls/Simulations/20201109Lorin/SimulationMain.Revisions.vs3.R $X_File1 $Genes_File1 $Covars_File1 $Genes_File2 $Output1_File1 $Seed1 $Datasets1 $PVE1 $Rho1 $PCs_var1 $ncausaltotal1 $nCausal1a $nCausal2a $nCausal3a
+			sbatch -t 72:00:00 -n 2 -N 1-1 --mem 30g --account=ccmb-condo -o ${Output1_Slurm1}.slurm.output -e ${Output1_Slurm1}.slurm.error --comment "Sims $ancestry2 $o $PVE1 $Rho1 $PCs_var1 $ncausaltotal1 $nCausal1a $nCausal2a $nCausal3a" <(echo -e '#!/bin/sh'; echo -e "\nRscript /users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2AdditiveMdls/Simulations/20201109Lorin/SimulationMain.Revisions.vs4.dev.R $X_File1 $Genes_File1 $Covars_File1 $Genes_File2 $Output1_File1 $Seed1 $Datasets1 $PVE1 $Rho1 $PCs_var1 $ncausaltotal1 $nCausal1a $nCausal2a $nCausal3a")
+#			echo Rscript /users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2AdditiveMdls/Simulations/20201109Lorin/SimulationMain.Revisions.vs4.R $X_File1 $Genes_File1 $Covars_File1 $Genes_File2 $Output1_File1 $Seed1 $Datasets1 $PVE1 $Rho1 $PCs_var1 $ncausaltotal1 $nCausal1a $nCausal2a $nCausal3a
 		done;
         done; done; done; done; done;
 done
-
-
-
 
 #                PVE1=.6
 #                Rho1=.8
@@ -19072,15 +19072,15 @@ for j in `cat <(echo $UKBioBankPopsRnd2 | perl -lane 'print join("\n", @F);') | 
 	ancestry1=`echo $j | perl -ane 'my @vals1 = split(/;/, $F[0]); print $vals1[0];'`; ancestry2=`echo $j | perl -ane 'my @vals1 = split(/;/, $F[0]); print $vals1[1];'`; AncSeed1=`echo $j | perl -ane 'my @vals1 = split(/;/, $F[0]); print $vals1[3];'`
 	echo $ancestry1 $ancestry2 $AncSeed1
 
-	for pve2 in `cat <(echo ".6 .8" | perl -lane 'print join("\n", @F);') | tail -n 1`; do for rho2 in `cat <(echo ".5 .8" | perl -lane 'print join("\n", @F);') | head -n 1`; do for pcvar2 in `cat <(echo "0 .1" | perl -lane 'print join("\n", @F);') | head -n 1`; do for ncaustot2 in `cat <(echo ".05 .1 .25 .5 .75 1" | perl -lane 'print join("\n", @F);') | head -n 3`; do for ncaus2a2 in `cat <(echo "2 5 7 12 60 80" | perl -lane 'print join("\n", @F);') | tail -n 2`; do
+	for pve2 in `cat <(echo ".6 .8" | perl -lane 'print join("\n", @F);') | tail -n 1`; do for rho2 in `cat <(echo ".5 .8" | perl -lane 'print join("\n", @F);') | head -n 1`; do for pcvar2 in `cat <(echo "0 .1" | perl -lane 'print join("\n", @F);') | head -n 1`; do for ncaustot2 in `cat <(echo ".05 .1 .25 .5 .75 1" | perl -lane 'print join("\n", @F);') | head -n 3`; do for ncaus2a2 in `cat <(echo "2 5 7 5 10" | perl -lane 'print join("\n", @F);') | tail -n 2`; do
 		for o in {1..5}; do
 			PVE1=$pve2
 			Rho1=$rho2
 			PCs_var1=$pcvar2
 			ncausaltotal1=$ncaustot2
-			nCausal1a=20
+			nCausal1a=2
 			nCausal2a=$ncaus2a2
-			nCausal3a=`echo "200 - $nCausal1a - $nCausal2a" | bc -l`
+			nCausal3a=`echo "30 - $nCausal1a - $nCausal2a" | bc -l`
 			Output1_Path="/users/mturchin/LabMisc/RamachandranLab/InterPath/Vs1/Analyses/Rnd2AdditiveMdls/Simulations/20201109Lorin/Null/Results/$ancestry2"
 			Output1_File1="${Output1_Path}/ukb_chrAll_v3.${ancestry2}.QCed.reqDrop.QCed.dropRltvs.PCAdrop.sort.ImptHRC.dose.100geno.ForSimulations.chr16.Results._${PVE1}_${Rho1}_${PCs_var1}_${ncausaltotal1}_${nCausal1a}_${nCausal2a}_${nCausal3a}.Run${o}"; Output1_ROCs1="${Output1_Path}/ROCs/ukb_chrAll_v3.${ancestry2}.QCed.reqDrop.QCed.dropRltvs.PCAdrop.sort.ImptHRC.dose.100geno.ForSimulations.chr16.Results._${PVE1}_${Rho1}_${PCs_var1}_${ncausaltotal1}_${nCausal1a}_${nCausal2a}_${nCausal3a}.Run${o}";
 			GenesPulled1="${Output1_File1}.Results.nGenes.txt"
---
+++ SimulationMain.Revisions.vs4.dev.R
@@ -0,0 +1,145 @@
+#library("MAPITR")
+library("devtools")
+devtools::load_all("/users/mturchin/LabMisc/RamachandranLab/MAPITR")
+
+args <- commandArgs()
+#print(args)
+X.File <- args[6]
+Genes.File <- args[7]
+Covars.File <- args[8]
+Genes.Analysis.File <- args[9]
+Output1.File <- args[10]
+seed.value <- as.numeric(as.character(args[11]))
+n.datasets <- as.numeric(as.character(args[12]))
+pve <- as.numeric(as.character(args[13]))
+rho <- as.numeric(as.character(args[14]))
+pc.var <- as.numeric(as.character(args[15]))
+#ngenes <- as.numeric(as.character(args[16]))
+#nsnps <- as.numeric(as.character(args[16]))
+ncausaltotal <- as.numeric(as.character(args[16]))
+ncausal1 <- as.numeric(as.character(args[17]))
+ncausal2 <- as.numeric(as.character(args[18]))
+ncausal3 <- as.numeric(as.character(args[19]))
+#ncausal3 = 1-(ncausal1+ncausal2) #Remaining SNPs to be allocated to G3
+#nmask <- as.numeric(as.character(args[20]))
+
+#set.seed(seed.value)
+
+X <- as.matrix(read.table(X.File, header=T));
+Genes <- read.table(Genes.File, header=F);
+Covars <- read.table(Covars.File, header=T);
+Genes.Analysis <- read.table(Genes.Analysis.File, header=F);
+PCs <- as.matrix(Covars[,(ncol(Covars)-9):ncol(Covars)]);
+
+#Genes <- Genes[1:20,]
+Genes.Analysis <- Genes.Analysis[1:20,]
+
+Xmean=apply(X, 2, mean); Xsd=apply(X, 2, sd); X=t((t(X)-Xmean)/Xsd)
+ind = nrow(X); nsnp = ncol(X)
+
+### Run the Analysis ###
+for(i in 1:n.datasets) {
+  
+  #Select Causal Pathways
+
+  genes.ids = 1:nrow(Genes)
+#  s1.genes.ids <- sample(genes.ids, ncausal1, replace=F);
+  s1.genes.ids <- genes.ids[1:ncausal1];
+  s2.genes.ids <- sample(genes.ids[-s1.genes.ids], ncausal2, replace=F);
+  s3.genes.ids <- sample(genes.ids[c(-s1.genes.ids,-s2.genes.ids)], ncausal3, replace=F);
+  genes.pulled <- rbind(cbind(as.character(Genes[s1.genes.ids,1]), rep("Epi1", length(s1.genes.ids))), cbind(as.character(Genes[s2.genes.ids,1]), rep("Epi2", length(s2.genes.ids))), cbind(as.character(Genes[s3.genes.ids,1]), rep("Add", length(s3.genes.ids))));
+  s1 <- c(); s1.sizes <- c(); s2 <- c(); s2.sizes <- c(); s3 <- c(); s3.sizes <- c();
+  for (j in 1:length(s1.genes.ids)) {
+	gene.temp <- Genes[s1.genes.ids[j],];
+	gene.temp.SNPs <- unlist(strsplit(as.character(gene.temp[1,2]), ","));
+  	gene.temp.SNPs.pulled <- sample(gene.temp.SNPs, round(ncausaltotal * length(gene.temp.SNPs)), replace=F);
+	s1 <- c(s1, gene.temp.SNPs.pulled); s1.sizes <- c(s1.sizes, length(gene.temp.SNPs.pulled));
+  }
+  for (j in 1:length(s2.genes.ids)) {
+	gene.temp <- Genes[s2.genes.ids[j],];
+	gene.temp.SNPs <- unlist(strsplit(as.character(gene.temp[1,2]), ","));
+  	gene.temp.SNPs.pulled <- sample(gene.temp.SNPs, round(ncausaltotal * length(gene.temp.SNPs)), replace=F);
+	s2 <- c(s2, gene.temp.SNPs.pulled); s2.sizes <- c(s2.sizes, length(gene.temp.SNPs.pulled));
+  }
+  for (j in 1:length(s3.genes.ids)) {
+	gene.temp <- Genes[s3.genes.ids[j],];
+	gene.temp.SNPs <- unlist(strsplit(as.character(gene.temp[1,2]), ","));
+  	gene.temp.SNPs.pulled <- sample(gene.temp.SNPs, round(ncausaltotal * length(gene.temp.SNPs)), replace=F);
+	s3 <- c(s3, gene.temp.SNPs.pulled); s3.sizes <- c(s3.sizes, length(gene.temp.SNPs.pulled));
+  }
+  print(c(ncausaltotal,ncausal1,ncausal2,ncausal3,length(s1.genes.ids),length(s2.genes.ids),length(s3.genes.ids),length(s1),length(s2),length(s3)));
+  print(s1.sizes); print(s2.sizes); print(s3.sizes);
+  
+  ### Simulate the Additive Effects ###
+  SNPs.additive = c(s1,s2,s3);
+#  print(dim(X))
+#  print(SNPs.additive)
+  Xmarginal = X[,SNPs.additive]
+  beta=rnorm(dim(Xmarginal)[2])
+  y_marginal=c(Xmarginal%*%beta)
+  beta=beta*sqrt(pve*rho/var(y_marginal))
+  y_marginal=Xmarginal%*%beta
+  
+  ### Simulate Pairwise Interaction matrix ###
+  y_epi <- 0;
+  if ((ncausal1 > 0) && (rho < 1)) { 
+	  Xepi = c(); b = c();
+	  for(j in 1:length(s1)){
+	      Xepi = cbind(Xepi,X[,s1[j]]*X[,s2]) 
+	  }
+	  
+	  ### Simulate the Pairwise Effects ###
+	  beta=rnorm(dim(Xepi)[2])
+	  y_epi=c(Xepi%*%beta)
+	  beta=beta*sqrt(pve*(1-rho)/var(y_epi))
+	  y_epi=Xepi%*%beta
+   }
+
+  ### Define the effects of the PCs, if included in simulation ###
+  y_pcs <- 0;
+  if (pc.var > 0) { 
+	  beta=rnorm(dim(PCs)[2])
+	  y_pcs=c(PCs%*%beta)
+	  beta=beta*sqrt(pc.var/var(y_pcs))
+	  y_pcs=PCs%*%beta
+  }
+
+  ### Simulate the (Environmental) Error/Noise ###
+  y_err=rnorm(ind)
+  y_err=y_err*sqrt((1-pve-pc.var)/var(y_err))
+  
+  ### Simulate the Total Phenotypes ###
+  y=y_marginal+y_epi+y_pcs+y_err
+  y=(y-mean(y))/sd(y)
+  
+  ### Check dimensions ###
+  print(seed.value); X.dims <- dim(X); print(X.dims); print(length(y));
+  
+  ######################################################################################
+  ######################################################################################
+  ######################################################################################
+
+  #nmask = prcntg
+#  if (nmask > 0) {
+#	s1.s2.mask <- sample(c(s1,s2), (length(s1) + length(s2)) * nmask, replace=F);
+#	X <- subset(X, select=-1*s1.s2.mask);
+#	X <- X[,-which(colnames(X) %in% s1.s2.mask);
+#  }
+#  print(c(X.dims, length(s1.s2.mask), dim(X)));
+
+  ptm <- proc.time() #Start clock
+#  MAPITR_Output <- MAPITR(X,y,Genes.Analysis,Covariates=PCs) 
+  MAPITR_Output <- MAPITR(X,y,Genes.Analysis) 
+  print(proc.time() - ptm) #Stop clock
+#  print(head(MAPITR_Output$Results))
+
+  stats <- c(); stats <- rbind(stats, c(seed.value, n.datasets, pve, rho, pc.var, ncausaltotal, ncausal1, ncausal2, ncausal3)); stats <- rbind(stats, dim(genes.pulled)); stats <- rbind(stats, length(s1)); stats <- rbind(stats, length(s2)); stats <- rbind(stats, length(s3));
+
+  write.table(y, paste(Output1.File, ".Results.Pheno.txt", sep=""), quote=FALSE, col.name=FALSE, row.name=FALSE);
+  write.table(genes.pulled, paste(Output1.File, ".Results.nGenes.txt", sep=""), quote=FALSE, col.name=FALSE, row.name=FALSE);
+#  write.table(s1, paste(Output1.File, ".Simulation.nCausal1.txt", sep=""), quote=FALSE, col.name=FALSE, row.name=FALSE);
+#  write.table(s2, paste(Output1.File, ".Simulation.nCausal2.txt", sep=""), quote=FALSE, col.name=FALSE, row.name=FALSE);
+  write.table(stats, paste(Output1.File, ".Results.statistics.txt", sep=""), quote=FALSE, col.name=FALSE, row.name=FALSE);
+  write.table(MAPITR_Output$Results, paste(Output1.File, ".Results.Output.txt", sep=""), quote=FALSE, col.name=TRUE, row.name=FALSE);
+
+}
